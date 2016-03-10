5c9255a3dbb669830d30627491fe17e4940c5541
ural5151 <amgid7@mail.ru>
2016-03-11 02:39:48 +0500

    Added files via upload

---
+++ Kredit.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kredit;
+
+ 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+public class Kredit {
+    private JTextField pField;
+    private JLabel pLabel;
+    private JLabel kmLabel;
+    private JTextField termField;
+    private JLabel termLabel;
+    private JLabel litersLabel;
+    private JTextField priceField;
+    private JLabel priceLabel;
+    private JLabel rurLabel;
+    private JButton calcButton;
+    private JLabel resultLabel;
+    private JFrame mainFrame;
